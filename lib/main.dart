ea48c43e42f08b162291e978ea71b17d08897339
abdullahaahmed0539 <abdullahaahmed0539@gmail.com>
2021-05-03 21:35:08 +0500

    feat: usage of invisible widgets

--- lib/main.dart
+++ lib/main.dart
@@ -5,11 +5,25 @@ void main () => runApp(MyApp());
 
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) => MaterialApp(
-    home: Scaffold(
-      appBar: AppBar(title: Text('My first app'),),
-      body: Text('This is default text!'),
-    ),
-  );
-  
+  Widget build(BuildContext context) { 
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animal?',
+    ];
+    
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My first app'),
+        ),
+        body: Column(
+          children: [
+            Text('The question'),
+            ElevatedButton(child: Text('Answer 1'), onPressed: null,),
+            ElevatedButton(child: Text('Answer 2'), onPressed: null,),
+          ]
+        ),
+      ),
+    );
+  }
 }
